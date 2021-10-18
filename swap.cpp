bfda0ac817c88a62a53712f40ac07a2e2955f9f8
Shivani Verma <shivaniverma.ira@gmail.com>
2021-10-18 23:00:57 +0530

    swap function
    Oct 18

---
+++ swap.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap (int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b; 
+    *b = temp;
+    //printf("In func, a = %d b = %d\n", a, b); 
+    //cout<<"In func, a = "<<*a<<" and b = "<<*b<<endl;
+}  
+
+
+int main(){
+    //int x = 5, y = 7;  
+    int x,y =0;
+    cin>>x>>y;
+    cout<<"before swap, x = "<<x<<" y = "<<y<<endl;
+    swap(&x, &y); 
+    //&x = x ka address
+    //*&x= x ka value
+    //int *a = &x -> a is pointer pointing to x   
+    //printf("In main, x = %d y = %d\n", x, y);
+    cout<<"After swap, x = "<<x<<" y = "<<y<<endl;
+    return 0;
+}
+
+// %d = integer, %f = float, %s = string, %u = unsigned
+// \n = enter (endl)
